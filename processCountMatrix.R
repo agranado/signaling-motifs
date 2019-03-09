eda8a9b07d7a3274983748e29f76e5644dafab70
agranado <alejandro.granados.c@gmail.com>
2019-03-08 18:01:44 -0800

    manual normalization of Seurat data

--- processCountMatrix.R
+++ processCountMatrix.R
@@ -123,6 +123,14 @@ raw.data  = raw.data[,high.count.cells]
 
 
 
+
+######lets save the total count of reads,
+# Seurat normalizes the data assuming that genes in the matrix are transcriptome
+# therefore  gene_i/sum(gene)  is biased when taking only a subset of genes and we get weird things
+
+  total.reads = Matrix::colSums(raw.data)
+
+
   bmp.all = pathway.genes(pathway = "bmp")
   bmp.indexes=match(bmp.all,rownames(raw.data))
 
@@ -146,9 +154,15 @@ raw.data  = raw.data[,high.count.cells]
 cell.meta.filter = cell.meta.data[colnames(raw.data), ]
 
 
+#take the total read count only for the cells that remain
+total.reads = total.reads[colnames(raw.data)]
+
 #last step: filter duplicated cells (with exact same count profiles)
 duplicated.cells = duplicated(t(as.matrix(raw.data)))
 raw.data=raw.data[,!duplicated.cells]
+#remove the duplicate from the metadata
+cell.meta.filter=cell.meta.filter[colnames(raw.data),]
+total.reads = total.reads[colnames(raw.data)]
 #-------------------------------------------------------------------------------
 
 # # # # ## SEURAT OBJECT
@@ -156,6 +170,7 @@ tiss <- CreateSeuratObject(raw.data = raw.data)
 
 tiss <- AddMetaData(object = tiss, cell.meta.data)
 tiss <- AddMetaData(object = tiss, percent.ercc, col.name = "percent.ercc")
+tiss<- AddMetaData(object = tiss, total.reads,col.name = 'total.reads')
 # Change default name for sums of counts from nUMI to nReads
 colnames(tiss@meta.data)[colnames(tiss@meta.data) == 'nUMI'] <- 'nReads' # this is not UMI data so Seurat calculates only the number of reads
 
@@ -189,9 +204,20 @@ GenePlot(object = tiss, gene1 = "nReads", gene2 = "nGene", use.raw=T)
 
 #filter cells that have less that 50000 reads, or that have more than 500 genes with zero reads
 tiss <- FilterCells(object = tiss, subset.names = c("nGene", "nReads"), low.thresholds = c(2, 50))
-# # #Normalize the data, then center and scale.
 
+
+# # #Normalize the data, then center and scale.
+# Equivalent to
+# # # r = t(t(raw.data)/Matrix::colSums(raw.data))
+#     log(r[1:3,1:3] * 1000000 + 1)
+
+#make Seurat think we normalized using their method...
 tiss <- NormalizeData(object = tiss, scale.factor = 1e6) #default normalization by Seurat
+
+#lets try to force normalization by hand :
+manual.norm = log(t( t(tiss@raw.data)/total.reads  ) * 1000000 +1)
+tiss@data<-manual.norm
+
 tiss <- ScaleData(object = tiss)
 tiss <- FindVariableGenes(object = tiss, do.plot = TRUE, x.high.cutoff = Inf, y.cutoff = 0.5,num.bin =10)
 #for BMP we see 11 variable genes
@@ -216,7 +242,7 @@ PCElbowPlot(object = tiss, num.pc = 10)
 # Choose the number of principal components to use.
 # ```{r}
 # Set number of principal components.
-n.pcs = 10
+n.pcs = 6
 # ```
 #
 # The clustering is performed based on a nearest neighbors graph.
