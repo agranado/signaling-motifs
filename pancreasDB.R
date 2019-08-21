88dd283d73d5103b9963634a15ade3703713f9f2
Alejandro <alejandro.granados.c@gmail.com>
2019-08-21 10:51:43 -0700

    Pancreas scripts fully working for Krentz and Byrnes datasets

--- pancreasDB.R
+++ pancreasDB.R
@@ -22,7 +22,7 @@ pancreas.files = paste(panglao.path, pancreas.files, sep = "")
 conditions = c("E15_progenitors_Krentz","E18_progenitors_Krentz" ,"E18_green_Krentz","E15_yellowGreen_Krentz","E18_yellow_Krentz")
 #panglao has some errors in the annotation!
 corrected_conditions = c("E15_red",  "E18_green", "E18_red","E15_yellow_green")
-
+# NOTE:  After Aug 19th. The list was saved and re-named to match the original annotation order
 
 
 
@@ -30,7 +30,11 @@ notch.genes = c("Dll1",   "Dll3"   ,"Dll4",   "Dtx1",   "Jag1"  , "Jag2", "Notch
 bmp.receptors<-c( "Bmpr1a" ,"Bmpr1b" ,"Acvr1"  ,"Acvrl1" ,"Acvr1b" ,"Tgfbr1" ,"Acvr1c" ,"Acvr2a", "Acvr2b", "Bmpr2" ,"Tgfbr2")
 
 
-load.PanglaoDB<-function(input_file = "", min_colSums = 3, max_colSums = 6, max_rowSums = 6,
+# # # # # # # # # # # # # #
+# Main Functions:
+#
+
+load.PanglaoDB<-function(input_file = "", min_colSums = 3, max_colSums = 6, min.percent = 0.05, max_rowSums = 6,
                             k = 10, data_type = "10x", plot.all = T){
 
   load(input_file)
@@ -43,7 +47,7 @@ load.PanglaoDB<-function(input_file = "", min_colSums = 3, max_colSums = 6, max_
   }
 
   #minimum percentage of cells in which a gene should be expresed
-  min.percent = 0.05
+
   #Manually Filtering cells
   if(plot.all){
     x11()
@@ -120,15 +124,49 @@ normaliseAndImpute<-function(sm2_filtered = c(), k_magic = 10,plot.all = F){
 
 }
 
-plotPathwayMagic<-function(sce_magic_data= c() , gene.list = bmp.receptors, cut_k = 10, main_title = ""){
+plotPathwayMagic<-function(sce_magic_data= c() , gene.list = bmp.receptors,
+                      cut_k = 10, main_title = "",ann_df = c()){
+
+    #22 distinct colors
+    distinct_colors = c('#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4',
+                        '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff',
+                        '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1',
+                        '#000075', '#808080', '#ffffff', '#000000')
 
     gene.names = row.names(sce_magic_data)
-    p2 =pheatmap(sce_magic_data[which(gene.names %in% gene.list),] ,cluster_rows = F,cutree_cols = cut_k,
+
+    #with annotation for colors and cell types
+    # List of color must match a given cell type
+    if(length(ann_df)>0){
+
+      mycolors <-  distinct_colors[1:length(unique(ann_df$cell_type))]
+      names(mycolors) <- unique(ann_df$cell_type)
+      mycolors <- list(cell_type = mycolors) # Here use the name of the variable you want to annotate!
+
+      # subset genes from the list of pathway-genes
+      # Sort then alphabetically
+      # Create a heatmap with 10 cuts and save it (will be returned by the function)
+      p2 =pheatmap(sce_magic_data[sort(gene.names[which(gene.names %in% gene.list)]),] ,cluster_rows = F,cutree_cols = cut_k,
+            show_colnames = F,main = main_title,annotation_col = ann_df,
+              annotation_colors = mycolors)
+
+    #No annotations
+    }else{
+      p2 =pheatmap(sce_magic_data[which(gene.names %in% gene.list),] ,cluster_rows = F,cutree_cols = cut_k,
             show_colnames = F,main = main_title)
+    }
 
     return(p2[[4]])
-
 }
+
+
+
+# # # # # # # # # # # # # # # # #
+# Krentz 2018 Stem Cell Reports
+# Pancreas Progenitors 10x sc RNA sequ
+# E15.5 18.5
+# Data from Panglao and from NCBI (All datasets were cross-validated for annotation)
+# Metadata: From NCBI
 
 #we can directly download the data from paper's website to compare
 # There are some errors in the annotation of PanglaoDB for this particular dataset
@@ -136,28 +174,112 @@ plotPathwayMagic<-function(sce_magic_data= c() , gene.list = bmp.receptors, cut_
 # There is also a table of number of cells per sample in the paper, we want to use that to compare to
 krentz_paper_files=grep("normalized",list.files("/Users/alejandrog/Downloads/Krentz_2019/"),value = T)
 krentz_paper_files = paste("Downloads/",krentz_paper_files,sep ="")
+#Returns a list of lists: [[1]] is a list of magic_matrices [[2]] is a list of the associated metadata
+loadFromPaper<-function(krentz_paper_files = krentz_paper_files,res=list()){
 
-loadFromPaper<-function(file_name = "Downloads/Krentz_2019/GSM3402516_E18_green_normalized_counts_matrix.txt.gz",res=list()){
+  for(i in 1:length(krentz_paper_files)){
+      file_name = krentz_paper_files[i]
+      aa<-read.table(file_name,row.names = 1,header = T)
+      aa<-t(aa)
+      #this has metadata
+      # first 4 columns are the meta data
+      aa_data = aa[5:dim(aa)[1],]
+      ncell = ncol(aa_data)
+      ngene = nrow(aa_data)
+      # Data comes as string so we need to convert it to numeric
+      aa_data<- matrix(mapply(aa_data, FUN=as.numeric),  ncol=ncell, nrow=ngene)
+      row.names(aa_data)<-row.names(aa)[-c(1:4)]
+      colnames(aa_data) = colnames(aa)
 
-  aa<-read.table(file_name,row.names = 1,header = T)
-  aa<-t(aa)
-  #this has metadata
-  aa_data = aa[5:dim(aa)[1],]
-  ncell = ncol(aa_data)
-  ngene = nrow(aa_data)
-  aa_data<- matrix(mapply(aa_data, FUN=as.numeric),  ncol=ncell, nrow=ngene)
-  row.names(aa_data)<-row.names(aa)[-c(1:4)]
-  colnames(aa_data) = colnames(aa)
+      # In this file, the names of cells have two underscores, and therefore the index we want is 4 instead of 3
+      if(length(grep("E15_yellow_green",file_name))>0) barcode_index = 4 else barcode_index = 3
 
-  # In this file, the names of cells have two underscores, and therefore the index we want is 4 instead of 3
-  if(grep("E15_yellow_green",file_name)) barcode_index = 4 else barcode_index = 3
+      colnames_simple = str_split(str_split(colnames(aa_data),"_",simplify=T )[,barcode_index], "-", simplify=T)[,1]
+      colnames(aa_data)<-colnames_simple
 
-  colnames_simple = str_split(str_split(colnames(aa_data),"_",simplify=T )[,barcode_index], "-", simplify=T)[,1]
+      #let's compile the meta data
+      meta_krentz<-aa[1:4,]
+      meta_krentz<-as.data.frame(meta_krentz)
 
-  colnames(aa_data)<-colnames_simple
+
+
+      krentz_magic[[i]] = aa_data
+      krentz_meta[[i]]  = meta_data
+    }
 
   return(aa_data)
 }
+
+#NOTE E15_red has not cell_type annotation
+
+loadMetaKrentz<-function(file_name = krentz_paper_files[1]){
+  #This function only applies for that specific file format
+
+  aa<-read.table(file_name,row.names = 1,header = T)
+  aa<-t(aa)
+
+  if(length(grep("E15_yellow_green",file_name))>0) barcode_index = 5 else barcode_index = 4
+
+  sample_name = paste(str_split(str_split(file_name,"normalized",simplify = T)[,1],"_",simplify=T)[,3:barcode_index],collapse = "_")
+
+  meta_krentz<-aa[1:4,]
+  meta_krentz<-as.data.frame(meta_krentz)
+
+  meta_krentz_E18_green<-t(meta_krentz)
+  meta_krentz_E18_green<-as.data.frame(meta_krentz_E18_green)
+  meta_krentz_E18_green$simple_name<-str_split(str_split(colnames(meta_krentz),"_",simplify = T)[,barcode_index-1],"-",simplify = T)[,1]
+
+  return(list(meta_krentz_E18_green))
+
+}
+
+
+# # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # #
+# Byrnes 2018 Nat Comm
+# Whole Pancreas 10x sc RNA sequ
+# E12.5, 14.5 17.5
+# Data from Panglao  17.5 (UMIs) and from FigShare 14.5 (official, as Seurat object)
+# From FigShare, we have metadata
+
+# This function will load and process the Seurat objects available in FigShare
+# Seruat objects for this paper are very old versions and are NOT compatible with Seurat 3
+loadByrnesFigShare<-function(file_name="Downloads/Byrnes2018/E14_allcells_seur_ob.Rdata"){
+
+  load(file_name)
+  # EXtract raw and meta data
+  byrnes_E14_meta<-seurat_ob@data.info
+  byrnes_E14_raw = seurat_ob@raw.data
+  # My function for SCRAN  + MAGIC
+  byrnes_E14_magic <- normaliseAndImpute(byrnes_E14_raw)
+
+
+  # Annotation of byrnes data object:
+  byrnes_E14_ann = data.frame(cell_type = byrnes_E14_meta$assigned_ident[ which(row.names(byrnes_E14_meta) %in% colnames(byrnes_E14_raw)) ]  )
+
+
+  # Returns the imputed data matrix for this object
+  return(list(byrnes_E14_magic,byrnes_E14_ann))
+
+  # From here: execture the plotting function
+  # with annotation
+  # byrnes_E14_p<-plotPathwayMagic(byrnes_E14_magic,main_title = "Byrnes E14 (2batches)",ann_df = byrnes_E14_ann)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 old.code<-function(){
