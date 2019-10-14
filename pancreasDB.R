09c627066ceba810d58702556aff31d810fbbba8
agranado <alejandro.granados.c@gmail.com>
2019-10-14 11:15:13 -0700

    changes in linux machine when doing magic on tabula muris. Also when trying out scMerge...

--- pancreasDB.R
+++ pancreasDB.R
@@ -92,25 +92,25 @@ load.PanglaoDB<-function(input_file = "", min_colSums = 3, max_colSums = 6, min.
 #CPU time, Mac 8min single core for pancreas.files[1]
 
 normalizeSCRAN<-function(sm2_filtered){
-  # SCRAN normalization
-  # We need a SCE object so we will convert our matrix to that,
-  # For MAGIC, we need a UMI matrix so we will convert to Seurat so
-  # we can extract the count matrix from there (not elegant)
+    # SCRAN normalization
+    # We need a SCE object so we will convert our matrix to that,
+    # For MAGIC, we need a UMI matrix so we will convert to Seurat so
+    # we can extract the count matrix from there (not elegant)
 
-  # The counts slot can be filled with RPKM values, since it is used for dropout correction
-  sce<-SingleCellExperiment(list(counts = sm2_filtered))
-  clusters <- quickCluster(sce, min.size=100) # TAKES a long time
-  sce <- computeSumFactors(sce, cluster=clusters)
-  # Now we can normalize the data
-  # Compute normalised expression values from count data in a SingleCellExperiment object, using the size factors stored in the object.
-  sce<-scater::normalize(sce)
+    # The counts slot can be filled with RPKM values, since it is used for dropout correction
+    sce<-SingleCellExperiment(list(counts = sm2_filtered))
+    clusters <- quickCluster(sce, min.size=100) # TAKES a long time
+    sce <- computeSumFactors(sce, cluster=clusters)
+    # Now we can normalize the data
+    # Compute normalised expression values from count data in a SingleCellExperiment object, using the size factors stored in the object.
+    sce<-scater::normalize(sce)
 
-  #let's convert to Seurat:
-  sce.seurat <- as.Seurat(sce)
+    #let's convert to Seurat:
+    sce.seurat <- as.Seurat(sce)
 
-  norm_count_matrix = as.matrix(sce.seurat[['RNA']]@data )
+    norm_count_matrix = as.matrix(sce.seurat[['RNA']]@data )
 
-  return(norm_count_matrix)
+    return(norm_count_matrix)
 
 }
 
