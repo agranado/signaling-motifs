3b30b50c2f47537363b79d792f278199d32b4652
agranado <alejandro.granados.c@gmail.com>
2021-01-22 13:59:30 -0800

    New functions for second round of clustering

--- motifs_figure1_global.R
+++ motifs_figure1_global.R
@@ -77,6 +77,46 @@ clusterPathway<- function(
 	return(list(heatmap = pp, colors = colors, counts =  count_mat, fil_rows =  fil_rows, dist_mat  = spectral_dist))
 }
 
+# function for recursive clustering
+# This version does not need all the pre-processing steps, since it is called after clusterPathway
+# should be quicker Jan 22nd 2021
+reClusterPathway<- function(count_mat = matrix() ,
+                            meta_data = data.frame(),
+                            k = 20,
+                            spec_kernel = 'density',
+                            spec_method =2
+                           ){
+    # Everything is fine as long as we keep the global cluster labels
+    # We need:
+    #  1. count matrix (already normalized)
+    #  2. meta data with global cluster
+    #  3. This will output the metadata with the cluster label
+
+      # 1. Run spectrum distance matrix
+    spec_res = Spectrum(t(count_mat), maxk = k, showres = F,
+                        method = spec_method,
+                        kerneltype = spec_kernel)
+
+    # 5. Manual spectral clustering
+    manual_res = manualSpectral2(A = spec_res$similarity_matrix, labels = meta_data, k_opt =k_opt)
+    # Plot heatmap with spectral matrix
+    spectral_dist = manual_res[[3]]
+
+    # 6. Cluster eigen-vector and create dendrogram
+    p_clust <- pheatmap(t(count_mat),
+                        clustering_distance_cols = spectral_dist,
+                        clustering_method = 'ward.D2',
+                        silent = T)
+
+    # 7. Label cells with pathway state
+    meta_data$pathway_clust = rep(NA, dim(meta_data)[1])
+
+    motif_labels<-cutree(p_clust$tree_col,k)
+    meta_data[names(motif_labels),]$pathway_clust = motif_labels
+    meta_data$pathway_clust <- as.character(meta_data$pathway_clust)
+
+    return(meta_data)
+}
 
 # This function performs a basic clustering with cosine distance on the raw dataset
 # It returns a list of filtered data points that can be further processed
@@ -208,4 +248,88 @@ plotMotif3D <- function(p_clust = p , which_motif = 1,
 
 		return(scatter.labeled)
 	}
+}
+
+
+# Spectral clustering
+# Custom method that takes the distance matrix output from Spectrum
+manualSpectral2 <-function(A = c(), labels = data.frame, min_dist_adj = 0 , norm_laplacian= T, k_gmm = 10, k_opt = 12, filter_Qt  =F ){
+
+
+    Q_t = A
+    # 4. Make Degree matrix D: row sums of Q_t
+    D = diag(rowSums(Q_t), nrow =dim(A)[1], ncol = dim(A)[2])
+
+
+
+      if(filter_Qt){
+        Adj_mat = Q_t # from similarity matrix
+        diag(Adj_mat)<-0 # Adjacency is zero at diagonal
+        Adj_mat[Adj_mat<min_dist_adj] = 0 # binarize the matrix, filter for data.points with low similarity
+        Adj_mat[Adj_mat>0] = 1 # Everything else becomes 1 to indicate a connection between those points
+      }else{
+        Adj_mat = Q_t # alternative, just take the similarity matrix directly with no filtering
+        #diag(Adj_mat) = 0
+      }
+
+      # Degree matrix
+      Deg_mat = diag(rowSums(Adj_mat),nrow = dim(Adj_mat)[1], ncol = dim(Adj_mat)[2]) # Degree matrix comes now naturally
+
+      L = Deg_mat - Adj_mat # Unnormalized graph laplacian (most general form)
+
+      if(norm_laplacian){
+
+        # Here we can compute the normalized Laplacian
+        # matrix power operator: computes M^power (M must be diagonalizable)
+        "%^%" <- function(M, power)
+            with(eigen(M), vectors %*% (values^power * solve(vectors)))
+
+        L <- (Deg_mat %^% (-1/2)) %*% L %*% (Deg_mat %^% (-1/2))  # normalized Laplacian
+
+      }
+
+
+      evL <- eigen(L, symmetric=TRUE) # Eigen-decomposition
+
+
+      evL$values %>% diff() -> eigen_diff #optimal N of clusters
+
+      # 7. Identify optimal k of clusters based on eigen-gap
+      # Consider only the k top eigenvectors
+
+      if(k_opt ==0){
+        k_opt = which.min(eigen_diff) + 1 # diff_ removes the first element
+      }
+      #X = evL$vectors[,1:10] # Select only top 10 eigen vectors
+      # with the parameter k_opt we specify how many eigenvector we want to consider
+      # the coice of eigenvectors will almost dictate the number of clusters
+      X   <- evL$vectors[,(ncol(evL$vectors)-k_opt+1):ncol(evL$vectors)]
+
+
+      # Eigen-matrix normalization
+      Y = matrix(0, dim(X)[1], dim(X)[2])
+      # Normalize eigen vectors
+      for(i in 1:dim(X)[1]){
+          for(j in 1:dim(X)[2]){
+              Y[i,j] = X[i,j] / sqrt( sum( X[i,]^2) )
+          }
+      }
+
+
+      # 8. Gaussian mixture on eigenvector space
+      gmm = GMM(Y, k_gmm ,seed_mode = "random_subset", km_iter = 10,
+                em_iter = 10, verbose = F)
+      pr = predict_GMM(Y, gmm$centroids, gmm$covariance_matrices, gmm$weights)
+
+      # 9. Assign labels
+      # labels = data.frame(manual =  pr$cluster_labels %>% as.character())
+      labels$manuel = pr$cluster_labels %>% as.character()
+      row.names(labels) = row.names(A)
+      row.names(Y) = row.names(A)
+
+
+
+
+      return(list(labels, Y, dist(Y)))
+
 }
